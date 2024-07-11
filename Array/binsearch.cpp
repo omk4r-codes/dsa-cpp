ec1e98d183935c7ab14f4221f1007758ca48276c
omk4r-codes <salunkeos21.comp@coeptech.ac.in>
2024-07-11 20:07:00 +0530

    setup

--- Array/binsearch.cpp
+++ Array/binsearch.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int binSearchIndex(vector<int> &v, int target){
+    int l= 0, r = v.size()-1;
+    while (l <= r)
+    {
+        int mid = l + (r - l)/2;
+        if(target == v[mid])
+            return mid;
+        if (target < v[mid])
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return -1;
+    
+}
+
 int main()
 {
-
+    vector<int> v = {12, 56, 78, 98, 123, 167, 190, 256, 678};
+    int n;
+    cin >> n;
+    cout << binSearchIndex(v, n);
     return 0;
 }
